102e01f6058fa585aef99201617b2a4105b7a11a
Florian PICARD <fpicard@openstudio.fr>
2015-02-20 10:19:28 +0100

    First

---
+++ I18n/AdminIncludes/fr_FR.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'Back Office Path' => 'Chemin du Back Office',
+    'Edit your back office prefix.' => 'Modifier le préfixe du back office',
+    'Save' => 'Enregistrer',
+);
---
+++ I18n/fr_FR.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'Activate this to test your new prefix. If it doesn\'t work you could rollback your changes (the link in the HTML content will not be replaced)' => 'À activer pour tester votre nouveau préfixe. Si il ne fonctionne pas, vous pourrez revenir en arrière (les liens dans le contenu HTML ne seront pas remplacés)',
+    'It will replaced the default <code>%prefix</code>' => 'Cela remplacera le préfixe par défaut : <code>%prefix</code>',
+    'The new prefix' => 'Le nouveau préfixe',
+    'URL should only use alpha numeric, - and _ characters' => 'L\'URL doit uniquement être composée de caractères alpha-numérique, - et _ ',
+    'Use also the default prefix' => 'Utiliser également le préfixe par défaut',
+);
